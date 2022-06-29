1a09679e6f472f381a896e8d76fc949393083a5c
unknown <belcita814@gmail.com>
2022-06-29 14:10:32 -0300

    Update

---
+++ Back End/paqueteria/src/main/java/com/dh/paqueteria/PaqueteController.java
@@ -0,0 +1,41 @@
+package com.dh.paqueteria;
+
+import com.dh.paqueteria.model.Paquete;
+import com.dh.paqueteria.service.PaqueteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/paquetes")
+public class PaqueteController {
+
+    private PaqueteService paqueteService;
+
+    public PaqueteController(PaqueteService paqueteService) {
+        this.paqueteService = paqueteService;
+    }
+
+    @PostMapping
+    public Paquete registar(@RequestBody Paquete paquete){
+        return paqueteService.agregar(paquete);
+    }
+    @GetMapping
+    public List<Paquete> listar(){
+        return paqueteService.listar();
+    }
+    @GetMapping("/en-camino")
+    public List<Paquete> enCamino(){
+        return paqueteService.paquetesEnCamino();
+    }
+
+    @GetMapping("/entregado")
+    public List<Paquete> entregado(){
+        return paqueteService.paquetesEntregado();
+    }
+
+    @GetMapping("/por-recoger")
+    public List<Paquete> porRecoger(){
+        return paqueteService.paquetesPorRecoger();
+    }
+}
